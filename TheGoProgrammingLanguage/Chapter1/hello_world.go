507be70c46852362a6eeda69340eadb7595bd76f
edervs <eedervs.97@gmail.com>
2020-06-29 20:17:28 -0500

    Hello World!

---
+++ TheGoProgrammingLanguage/Chapter1/hello_world.go
@@ -0,0 +1,8 @@
+package main // Package main defines a standalone executable program, not a library.
+
+import "fmt"
+
+func main() { // Like many, main is where the execution starts.
+	// Semicolon is not required if the there is just one statement in a line.
+	fmt.Println("Hello, World!")
+}
