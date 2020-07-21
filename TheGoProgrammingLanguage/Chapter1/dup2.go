b3ff881619d34d97443a233c116c2b0f877a89c6
edervs <eedervs.97@gmail.com>
2020-07-21 08:12:58 -0500

    Dupl andd exercise

--- TheGoProgrammingLanguage/Chapter1/dup2.go
+++ TheGoProgrammingLanguage/Chapter1/dup2.go
@@ -10,9 +10,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	countsPerFile := make(map[string]map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, countsPerFile, "stdin")
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -20,21 +21,32 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, countsPerFile, arg)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
+			fmt.Println("Word:")
 			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Println("Files:")
+			for filename, m := range countsPerFile[line] {
+				fmt.Printf("%d\t%s\n", m, filename)
+			}
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) { // Reference pass
+func countLines(f *os.File, counts map[string]int, countsPerFile map[string]map[string]int, filename string) { // Reference pass
 	input := bufio.NewScanner(f)
+	var line string
 	for input.Scan() {
-		counts[input.Text()]++
+		line = input.Text()
+		if counts[line] == 0 {
+			countsPerFile[line] = make(map[string]int)
+		}
+		counts[line]++
+		countsPerFile[line][filename]++
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
